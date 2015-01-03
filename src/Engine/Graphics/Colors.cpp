b4e9d3a4bc46803c3fbc86b70e86580515b5423b
Alexandre Dantas <eu@alexdantas.net>
2015-01-03 00:35:26 -0500

    Improved the way "Color" and "ColorPair" work

--- src/Engine/Graphics/Colors.cpp
+++ src/Engine/Graphics/Colors.cpp
@@ -2,6 +2,105 @@
 
 #include <cstdlib>				// strtol()
 
+// Single Color
+
+Color::Color(std::string name):
+	name(name),
+	red(-1),
+	green(-1),
+	blue(-1),
+	ncurses_color(COLOR_WHITE)
+{ }
+
+std::string Color::toString()
+{
+	if (! this->name.empty())
+		return this->name;
+
+	return (Utils::String::toString(this->red) + "," +
+	        Utils::String::toString(this->green) + "," +
+	        Utils::String::toString(this->blue));
+}
+
+Color Color::fromString(std::string str)
+{
+	if (str.find(",") == std::string::npos)
+	{
+		// Format is "name"
+		return Colors::name(str);
+	}
+	else
+	{
+		// Format "red,green,blue"
+		std::vector<std::string> v = Utils::String::split(str, ',');
+		if (v.size() != 3)
+			return Color();
+
+		for (size_t i = 0; i < v.size(); ++i)
+			v[i] = Utils::String::trim(v[i]);
+
+		int red   = Utils::String::to<int>(v[0]);
+		int green = Utils::String::to<int>(v[1]);
+		int blue  = Utils::String::to<int>(v[2]);
+
+		return Colors::rgb(red, green, blue);
+	}
+}
+
+
+// Color Pair
+
+
+ColorPair::ColorPair():
+	bold(false),
+	ncurses_pair(0)
+{ }
+
+ColorPair::ColorPair(Color foreground, Color background):
+	foreground(foreground),
+	background(background),
+	bold(false),
+	ncurses_pair(0)
+{ }
+
+ColorPair ColorPair::reverse()
+{
+	return Colors::pair(background, foreground);
+}
+
+std::string ColorPair::toString()
+{
+	std::string bold = (this->bold ?
+	                    "!" :
+	                    "");
+
+	return (foreground.toString() + "+" + background.toString() + bold);
+}
+
+ColorPair ColorPair::fromString(std::string str)
+{
+	bool is_bold = false;
+
+	size_t pos = str.find("!");
+	if (pos != std::string::npos)
+	{
+		is_bold = true;
+		str.erase(pos, 1);
+	}
+
+	std::vector<std::string> v = Utils::String::split(str, '+');
+
+	if (v.size() != 2)
+		return ColorPair();
+
+	Color fg = Color::fromString(v[0]);
+	Color bg = Color::fromString(v[1]);
+
+	return Colors::pair(fg, bg, is_bold);
+}
+
+// All Colors
+
 bool Colors::hasColors = false;
 
 bool Colors::init()
@@ -71,29 +170,39 @@ bool Colors::init()
 	return true;
 }
 
-Color Colors::rgb(short r, short g, short b)
+Color Colors::rgb(short red, short green, short blue)
 {
-	if (can_change_color() == FALSE)
-		return 0;
+	Color color;
+	color.name = ""; // since it's RGB
 
-	if (COLORS < 256)
-		return 0;
+	if ((can_change_color() == FALSE) ||
+	    (COLORS < 256))
+		return color;
 
 	static int color_no = 8;
 	color_no++;
 	if (color_no >= COLORS)
 		color_no = 8;
 
-	// init_color receives values from 0 to 1000
+	// `init_color` receives values from 0 to 1000,
+	// so we'll map from 255 to 1000
 	int expand = 1000/255;
 
-	init_color((color_no - 1), r*expand, g*expand, b*expand);
-	return (color_no - 1);
+	init_color((color_no - 1), red*expand, green*expand, blue*expand);
+	color.ncurses_color = (color_no - 1);
+
+	color.red   = red;
+	color.green = green;
+	color.blue  = blue;
+	return color;
 }
 Color Colors::hex(std::string hex)
 {
-	if (hex[0] != '#')   return 0; // sorry
-	if (hex.size() != 7) return 0; // #RRGGBB format
+	Color color;
+
+	if ((hex[0]     != '#') || // sorry
+	    (hex.size() != 7)) // #RRGGBB format
+		return color;
 
 	char col[3];
 	col[2] = '\0';
@@ -113,31 +222,44 @@ Color Colors::hex(std::string hex)
 	return Colors::rgb(r, g, b);
 }
 
-ColorPair Colors::pair(Color foreground, Color background, bool is_bold)
+ColorPair Colors::pair(Color& foreground, Color& background, bool is_bold)
 {
+	ColorPair pair(foreground, background);
+	if (is_bold)
+		pair.bold = true;
+
+	short fg = foreground.ncurses_color;
+	short bg = background.ncurses_color;
+
 	// Basic nCurses colors
-	if ((foreground < 8) && (background < 8))
+	if ((fg < 8) && (bg < 8))
 	{
-		if (background == COLOR_DEFAULT)
+		if (bg == COLOR_DEFAULT)
 		{
 			if (is_bold)
-				return COLOR_PAIR(64 + foreground) | A_BOLD;
+				pair.ncurses_pair = COLOR_PAIR(64 + fg) | A_BOLD;
 			else
-				return COLOR_PAIR(64 + foreground);
+				pair.ncurses_pair = COLOR_PAIR(64 + fg);
+
+			return pair;
 		}
 
 		if (is_bold)
-			return COLOR_PAIR(foreground*8 + background + 1) | A_BOLD;
+			pair.ncurses_pair = COLOR_PAIR(fg*8 + bg + 1) | A_BOLD;
 		else
-			return COLOR_PAIR(foreground*8 + background + 1);
+			pair.ncurses_pair = COLOR_PAIR(fg*8 + bg + 1);
+
+		return pair;
 	}
 
 	if (COLORS < 256)
 	{
 		if (is_bold)
-			return COLOR_PAIR(0) | A_BOLD;
+			pair.ncurses_pair = COLOR_PAIR(0) | A_BOLD;
 		else
-			return COLOR_PAIR(0);
+			pair.ncurses_pair = COLOR_PAIR(0);
+
+		return pair;
 	}
 
 	// Will create color pair
@@ -147,50 +269,56 @@ ColorPair Colors::pair(Color foreground, Color background, bool is_bold)
 	if (color_pair_no >= COLOR_PAIRS)
 		color_pair_no = 72;
 
-	init_pair((color_pair_no - 1), foreground, background);
+	init_pair((color_pair_no - 1), fg, bg);
 
 	if (is_bold)
-		return COLOR_PAIR(color_pair_no - 1) | A_BOLD;
+		pair.ncurses_pair = COLOR_PAIR(color_pair_no - 1) | A_BOLD;
 	else
-		return COLOR_PAIR(color_pair_no - 1);
+		pair.ncurses_pair = COLOR_PAIR(color_pair_no - 1);
+
+	return pair;
 }
-Color Colors::fromString(std::string str)
+Color Colors::name(std::string str)
 {
+	Color color;
+
 	if (str.empty())
-		return 255;
+		return color;
 
-	if (str == "default") return COLOR_DEFAULT;
-	if (str == "black")   return COLOR_BLACK;
-	if (str == "red")     return COLOR_RED;
-	if (str == "green")   return COLOR_GREEN;
-	if (str == "yellow")  return COLOR_YELLOW;
-	if (str == "blue")    return COLOR_BLUE;
-	if (str == "magenta") return COLOR_MAGENTA;
-	if (str == "cyan")    return COLOR_CYAN;
-	if (str == "white")   return COLOR_WHITE;
+	     if (str == "default") color.ncurses_color = COLOR_DEFAULT;
+	else if (str == "black")   color.ncurses_color = COLOR_BLACK;
+	else if (str == "red")     color.ncurses_color = COLOR_RED;
+	else if (str == "green")   color.ncurses_color = COLOR_GREEN;
+	else if (str == "yellow")  color.ncurses_color = COLOR_YELLOW;
+	else if (str == "blue")    color.ncurses_color = COLOR_BLUE;
+	else if (str == "magenta") color.ncurses_color = COLOR_MAGENTA;
+	else if (str == "cyan")    color.ncurses_color = COLOR_CYAN;
+	else if (str == "white")   color.ncurses_color = COLOR_WHITE;
 
-	// keep in mind this error code
-	return 255;
+	color.name = str;
+	return color;
 }
 
-ColorPair Colors::pairFromString(std::string foreground, std::string background, bool is_bold)
+ColorPair Colors::pair(std::string foreground, std::string background, bool is_bold)
 {
 	if (foreground.empty() || background.empty())
-		return 255;
+		return ColorPair();
 
-	short f = Colors::fromString(foreground);
-	short b = Colors::fromString(background);
+	Color f = Colors::name(foreground);
+	Color b = Colors::name(background);
 
 	return Colors::pair(f, b, is_bold);
 }
 
-void Colors::activate(WINDOW* window, Color foreground, Color background)
+void Colors::activate(WINDOW* window, Color& foreground, Color& background)
 {
-	Colors::pairActivate(window, Colors::pair(foreground, background));
+	ColorPair pair = Colors::pair(foreground, background);
+
+	Colors::pairActivate(window, pair);
 }
 
-void Colors::pairActivate(WINDOW* window, ColorPair color)
+void Colors::pairActivate(WINDOW* window, ColorPair& color)
 {
-	wattrset(window, color);
+	wattrset(window, color.ncurses_pair);
 }
 
--- src/Engine/Graphics/Colors.hpp
+++ src/Engine/Graphics/Colors.hpp
@@ -1,35 +1,164 @@
 #ifndef COLORS_H_DEFINED
 #define COLORS_H_DEFINED
 
+#include <Engine/Helpers/Utils.hpp>
+#include <Engine/Helpers/String.hpp>
+
 #include <ncurses.h>
 #include <string>
+#include <vector>
 
-typedef short  Color;
-typedef chtype ColorPair;
-
+/// Default color of the current terminal.
+///
+/// This is the color your terminal has - both for
+/// foreground and background.
 #define COLOR_DEFAULT -1
 
+/// Single color.
+/// Pretty much useless, only used with `ColorPair`.
+struct Color
+{
+	/// Human-readable Color name.
+	/// @note If it's empty, means use RGB values.
+	std::string name;
+
+	/// Red component of the color, from 0 to 255.
+	/// @note If it's -1, means use the name string.
+	int red;
+
+	/// Green component of the color, from 0 to 255.
+	/// @note If it's -1, means use the name string.
+	int green;
+
+	/// Blue component of the color, from 0 to 255.
+	/// @note If it's -1, means use the name string.
+	int blue;
+
+	/// Internal value for the Ncurses library.
+	/// Don't mess with it.
+	short ncurses_color;
+
+	/// Creates a new color from its human-readable name.
+	///
+	/// @note If you want to create a color from its
+	///       RGB values, use `Colors::rgb()`
+	Color(std::string name="white");
+
+	/// Returns a string representation of this Color.
+	/// @note It can be converted back using `fromString`.
+	///
+	/// The format is "red,green,blue".
+	std::string toString();
+
+	/// Makes a color according to a string
+	/// representation of a Color.
+	/// @note It must be a value returned from `toString`.
+	static Color fromString(std::string str);
+};
+
+/// Color pair (foreground and background).
+/// Each character can get shown with a `ColorPair`.
+struct ColorPair
+{
+	Color foreground;
+	Color background;
+
+	/// Is this color bold?
+	/// Meaning is it brighter than its normal counterpart?
+	bool bold;
+
+	/// Internal ncurses value for a color pair.
+	/// Don't mess with it.
+	chtype ncurses_pair;
+
+	/// Creates an empty ColorPair.
+	/// @note Defaults to "white" over "black"
+	ColorPair();
+
+	/// Creates a ColorPair from both Colors.
+	ColorPair(Color foreground, Color background);
+
+	/// Returns a new color pair switching this color pair's
+	/// foreground and background.
+	ColorPair reverse();
+
+	/// Creates a string representation of this ColorPair.
+	/// @note Use later with `fromString`.
+	///
+	/// The format is "foreground+background".
+	/// If the color is bold, its has '!' at the end
+	/// (like "foregroud+background!").
+	std::string toString();
+
+	/// Makes a ColorPair be according to a string
+	/// representation returned by `toString()`.
+	///
+	static ColorPair fromString(std::string str);
+};
+
+/// Stuff related to Colors on a global scale.
+/// Defines colors to display characters on the screen.
 ///
 namespace Colors
 {
+	/// Tells if this terminal has support for colors.
+	/// Big news: most likely it does.
+	///
+	/// @note It only gets updated after calling `Colors::init()`
+	///
+	extern bool hasColors;
+
+	/// Initialize support for colors on the terminal.
 	bool init();
 
-	Color rgb(short r, short g, short b);
+	/// Returns a new color with red green and blue components.
+	///
+	/// @note They go from 0 (none) to 255 (full)
+	///
+	Color rgb(short red, short green, short blue);
 
+	/// Returns a new color with hex notation.
+	///
+	/// For example:
+	/// - "#ff0000" is red
+	/// - "#ff00ff" is magenta.
+	///
 	Color hex(std::string hex);
 
-	ColorPair pair(Color foreground, Color background, bool is_bold=false);
+	/// Returns a new color from a human-readable string.
+	/// For example: "red", "yellow" or "blue"
+	///
+	/// @note It is very limited for now, only having the 8
+	///       basic ncurses colors.
+	Color name(std::string str);
 
-	Color fromString(std::string str);
+	/// Returns a new `ColorPair` made with #foreground
+	/// and #background colors.
+	///
+	/// @note #is_bold usually brightens the color.
+	///
+	ColorPair pair(Color& foreground, Color& background, bool is_bold=false);
 
-	ColorPair pairFromString(std::string foreground, std::string background, bool is_bold=false);
+	/// Returns a new `ColorPair` made with human-readable strings.
+	///
+	/// @see Colors::pair()
+	/// @see Colors::fromString()
+	///
+	ColorPair pair(std::string foreground, std::string background, bool is_bold=false);
 
-	void activate(WINDOW* window, Color foreground, Color background);
+	/// Activates the colors #foreground and #background on a
+	/// ncurses #window.
+	///
+	/// It has the effect of making all subsequent prints having
+	/// this color pair.
+	///
+	void activate(WINDOW* window, Color& foreground, Color& background);
 
-	void pairActivate(WINDOW* window, ColorPair color);
-
-	// Var
-	extern bool hasColors;
+	/// Activates the color pair #color on a ncurses #window.
+	///
+	/// @see Colors::activate()
+	///
+	void pairActivate(WINDOW* window, ColorPair& color);
 }
 
 #endif //COLORS_H_DEFINED
